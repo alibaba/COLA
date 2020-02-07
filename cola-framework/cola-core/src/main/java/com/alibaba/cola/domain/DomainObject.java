a98058db3b5b478092db94b0fd9ca6db1e40fafe
wangliang <1991wangliang@gmail.com>
2020-02-07 21:38:22 +0800

    DomainObject

--- cola-framework/cola-core/src/main/java/com/alibaba/cola/domain/DomainObject.java
+++ cola-framework/cola-core/src/main/java/com/alibaba/cola/domain/DomainObject.java
@@ -1,11 +1,8 @@
 package com.alibaba.cola.domain;
 
+import com.alibaba.cola.common.ApplicationContextHelper;
 import com.alibaba.cola.event.EventBus;
 import com.alibaba.cola.repository.RepositoryBus;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author lorne
@@ -14,38 +11,15 @@ import java.util.List;
  */
 public abstract class DomainObject extends EntityObject {
 
-  protected EventBus eventBus;
+  protected static EventBus eventBus;
 
-  protected RepositoryBus repositoryBus;
+  protected static RepositoryBus repositoryBus;
 
-  public final void initEventBus(EventBus eventBus){
-    this.eventBus = eventBus;
-  }
-
-  public final void initPresentationBus(RepositoryBus repositoryBus){
-    this.repositoryBus = repositoryBus;
+  static {
+    eventBus = ApplicationContextHelper.getBean(EventBus.class);
+    repositoryBus = ApplicationContextHelper.getBean(RepositoryBus.class);
   }
 
   public void execute(){}
-
-  public final  <T extends DomainObject> T createDomain(Class<T> clazz, Object ... initargs) {
-    List<Class<?>> list =null;
-    if(initargs!=null){
-      list = new ArrayList<>();
-      for (Object obj:initargs){
-        list.add(obj.getClass());
-      }
-    }
-    Class<?>[] parameterTypes = list!=null?list.toArray(new Class[]{}):null;
-    try {
-      DomainObject domain = clazz.getDeclaredConstructor(parameterTypes).newInstance(initargs);
-      domain.initEventBus(eventBus);
-      domain.initPresentationBus(repositoryBus);
-      return (T)domain;
-    } catch (InstantiationException | IllegalAccessException |
-            InvocationTargetException | NoSuchMethodException e) {
-      throw new RuntimeException(e);
-    }
-  }
 
 }
