b2539d2b9bae97f6f64ff8f9c8b8cd5b277824fa
fulan.zjf <fulan.zjf@alibaba-inc.com>
2020-06-22 20:04:36 +0800

    cola3.0

--- cola-archetype-web/src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/executor/CustomerAddCmdExe.java
+++ cola-archetype-web/src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/executor/CustomerAddCmdExe.java
@@ -3,20 +3,17 @@
 #set( $symbol_escape = '\' )
 package ${package}.executor;
 
-
-import com.alibaba.cola.command.Command;
-import com.alibaba.cola.command.CommandExecutorI;
 import com.alibaba.cola.domain.DomainEventServiceI;
 import com.alibaba.cola.dto.Response;
 import com.alibaba.cola.exception.BizException;
 import ${package}.dto.CustomerAddCmd;
 import ${package}.dto.domainmodel.ErrorCode;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Command
-public class CustomerAddCmdExe implements CommandExecutorI<Response, CustomerAddCmd>{
 
-    @Override
+@Component
+public class CustomerAddCmdExe{
+
     public Response execute(CustomerAddCmd cmd) {
         //The flow of usecase is defined here.
         //The core ablility should be implemented in Domain. or sink to Domian gradually
