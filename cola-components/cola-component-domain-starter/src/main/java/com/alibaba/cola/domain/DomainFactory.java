e66632d8c87f697fbafc7820aefe70e408f78714
liuziyuan <liuziyuan.room@gmail.com>
2020-12-04 18:10:55 +0800

    更新注解

--- cola-components/cola-component-domain-starter/src/main/java/com/alibaba/cola/domain/DomainFactory.java
+++ cola-components/cola-component-domain-starter/src/main/java/com/alibaba/cola/domain/DomainFactory.java
@@ -17,7 +17,7 @@ public class DomainFactory {
      * 该方法调用默认构造器创建对象，如默认构造器没有为成员进行实例化。会自动new新的对象，以保证在使用聚合对象的时候，不会出现聚合内对象的空指针异常<br/>
      * 支持的类型有:<br/>
      * 使用@Entity注解的实体类<br/>
-     * 使用@ValueObject注解的值对象<br/>
+     * 使用@ValueObject注解的值对象，暂时未支持<br/>
      * 命名规范符合COLA规范的Entity，ValueObject分别以大写E和V结尾的对象
      * 集合List<T> 会默认new ArrayList<T>()<br/>
      * 集合Set<T> 会默认new HashSet<T>()<br/>
