09812439ecad983b223cc12dc416ec56f202df3a
Jerry Lee <oldratlee@gmail.com>
2021-01-06 16:09:32 +0800

    ! improve integration-test.sh
    - mvn clean before install
    - clean maven build and install of COLA in maven local repository
    - rename function: runCmd -> logAndRun

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+# NOTE about Bash Traps and Pitfalls:
+#
+# 1. DO NOT declare var as readonly if value is supplied by subshell!!
+#    for example: readonly var1=$(echo value1)
+#
+#    readonly declaration make exit code of assignment to be always 0,
+#      aka. the exit code of command in subshell is discarded.
+#      tested on bash 3.2.57/4.2.46
+
 set -eEuo pipefail
 
 # adjust current dir to project root dir
 cd "$(dirname "$0")/.."
-
 
 ################################################################################
 # constants
 ################################################################################
 
 # NOTE: $'foo' is the escape sequence syntax of bash
-readonly nl=$'\n' # new line
-readonly ec=$'\033' # escape char
+readonly nl=$'\n'        # new line
+readonly ec=$'\033'      # escape char
 readonly eend=$'\033[0m' # escape end
-
 
 ################################################################################
 # common util functions
@@ -43,19 +50,28 @@ headInfo() {
     colorEcho "0;34;46" ================================================================================
     yellowEcho "$*"
     colorEcho "0;34;46" ================================================================================
-    echo
 }
 
-runCmd() {
-    blueEcho "Run under work directory $PWD :$nl$*"
-    time "$@"
+logAndRun() {
+    local simple_mode=false
+    [ "$1" = "-s" ] && {
+        simple_mode=true
+        shift
+    }
+
+    if $simple_mode; then
+        echo "Run under work directory $PWD : $*"
+        "$@"
+    else
+        blueEcho "Run under work directory $PWD :$nl$*"
+        time "$@"
+    fi
 }
 
 die() {
     redEcho "Error: $*" 1>&2
     exit 1
 }
-
 
 ################################################################################
 # build util functions
@@ -76,40 +92,52 @@ MVN() {
         d=$(dirname "$d")
     done
 
-    runCmd "$d/$maven_wrapper_name" "${MVN_OPTIONS[@]}" "$@"
+    logAndRun "$d/$maven_wrapper_name" "${MVN_OPTIONS[@]}" "$@"
 }
 
+# Where is Maven local repository?
+#   https://mkyong.com/maven/where-is-maven-local-repository/
+
+# NOTE: DO NOT declare mvn_local_repository_dir var as readonly, its value is supplied by subshell.
+mvn_local_repository_dir="$(
+    cd cola-components/ &&
+        ./mvnw --no-transfer-progress help:evaluate -Dexpression=settings.localRepository |
+        grep '^/'
+)"
+[ -n "$mvn_local_repository_dir" ] || die "Fail to find find maven local repository directory: $mvn_local_repository_dir"
+
+echo "find maven local repository directory: $mvn_local_repository_dir"
 
 ################################################################################
 # CI operations
 ################################################################################
 
+headInfo "clean maven build and install of COLA in maven local repository:"
+logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/demo"
+logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/cola"
+logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/craftsman"
+
 (
     headInfo "CI: cola-components"
 
     cd cola-components/
-    MVN install
+    MVN clean install
 )
-
 
 (
     headInfo "CI: cola-archetypes"
 
     cd cola-archetypes/
-    MVN install
+    MVN clean install
 )
-
 
 (
     headInfo "CI: archetype:generate by cola-framework-archetype-service"
 
-    # NOTE: DO NOT declare archetypeVersion var as readonly!!
-    # readonly declaration make exit code of assignment to be always 0,
-    #   aka. the exit code of command in subshell is discarded.
-    #   tested on bash 3.2.57/4.2.46
+    # NOTE: DO NOT declare archetypeVersion var as readonly, its value is supplied by subshell.
     archetypeVersion=$(
         grep '<version>.*</version>' cola-archetypes/cola-archetype-service/pom.xml |
-        awk -F'</?version>' 'NR==1 {print $2}'
+            awk -F'</?version>' 'NR==1 {print $2}'
     )
 
     readonly demo_dir="cola-archetypes/target/cola-framework-archetype-service-demo"
@@ -131,17 +159,13 @@ MVN() {
     MVN install
 )
 
-
 (
     headInfo "CI: archetype:generate by cola-framework-archetype-web"
 
-    # NOTE: DO NOT declare archetypeVersion var as readonly!!
-    # readonly declaration make exit code of assignment to be always 0,
-    #   aka. the exit code of command in subshell is discarded.
-    #   tested on bash 3.2.57/4.2.46
+    # NOTE: DO NOT declare archetypeVersion var as readonly, its value is supplied by subshell.
     archetypeVersion=$(
         grep '<version>.*</version>' cola-archetypes/cola-archetype-web/pom.xml |
-        awk -F'</?version>' 'NR==1 {print $2}'
+            awk -F'</?version>' 'NR==1 {print $2}'
     )
 
     readonly demo_dir="cola-archetypes/target/cola-framework-archetype-web-demo"
@@ -163,10 +187,9 @@ MVN() {
     MVN install
 )
 
-
 (
     headInfo "CI: sample/craftsman"
 
     cd sample/craftsman/
-    MVN install
+    MVN clean install
 )
