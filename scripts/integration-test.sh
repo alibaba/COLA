69e7dfc0b90d919da5e718a9107196c3b49a0a56
Jerry Lee <oldratlee@gmail.com>
2021-01-07 12:27:26 +0800

    ! improve ci: use and test jdk8/9/11/15 of sdkman

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -10,112 +10,16 @@
 
 set -eEuo pipefail
 
+source "$(dirname "$(readlink -f "$BASH_SOURCE")")/common_build.sh"
+
 # adjust current dir to project root dir
 cd "$(dirname "$0")/.."
-
-################################################################################
-# constants
-################################################################################
-
-# NOTE: $'foo' is the escape sequence syntax of bash
-readonly nl=$'\n'        # new line
-readonly ec=$'\033'      # escape char
-readonly eend=$'\033[0m' # escape end
-
-################################################################################
-# common util functions
-################################################################################
-
-colorEcho() {
-    local color=$1
-    shift
-
-    # if stdout is the console, turn on color output.
-    [ -t 1 ] && echo "${ec}[1;${color}m$*${eend}" || echo "$*"
-}
-
-redEcho() {
-    colorEcho 31 "$@"
-}
-
-yellowEcho() {
-    colorEcho 33 "$@"
-}
-
-blueEcho() {
-    colorEcho 36 "$@"
-}
-
-headInfo() {
-    colorEcho "0;34;46" ================================================================================
-    yellowEcho "$*"
-    colorEcho "0;34;46" ================================================================================
-}
-
-logAndRun() {
-    local simple_mode=false
-    [ "$1" = "-s" ] && {
-        simple_mode=true
-        shift
-    }
-
-    if $simple_mode; then
-        echo "Run under work directory $PWD : $*"
-        "$@"
-    else
-        blueEcho "Run under work directory $PWD :$nl$*"
-        time "$@"
-    fi
-}
-
-die() {
-    redEcho "Error: $*" 1>&2
-    exit 1
-}
-
-################################################################################
-# build util functions
-################################################################################
-
-readonly -a MVN_OPTIONS=(
-    -V --no-transfer-progress
-)
-
-MVN() {
-    local maven_wrapper_name="mvnw"
-
-    local d="$PWD"
-    while true; do
-        [ "/" = "$d" ] && die "Fail to find $maven_wrapper_name!"
-        [ -f "$d/$maven_wrapper_name" ] && break
-
-        d=$(dirname "$d")
-    done
-
-    logAndRun "$d/$maven_wrapper_name" "${MVN_OPTIONS[@]}" "$@"
-}
-
-# Where is Maven local repository?
-#   https://mkyong.com/maven/where-is-maven-local-repository/
-
-# NOTE: DO NOT declare mvn_local_repository_dir var as readonly, its value is supplied by subshell.
-mvn_local_repository_dir="$(
-    cd cola-components/ &&
-        ./mvnw --no-transfer-progress help:evaluate -Dexpression=settings.localRepository |
-        grep '^/'
-)"
-[ -n "$mvn_local_repository_dir" ] || die "Fail to find find maven local repository directory: $mvn_local_repository_dir"
-
-echo "find maven local repository directory: $mvn_local_repository_dir"
 
 ################################################################################
 # CI operations
 ################################################################################
 
-headInfo "clean maven build and install of COLA in maven local repository:"
-logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/demo"
-logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/cola"
-logAndRun -s rm -rf "$mvn_local_repository_dir/com/alibaba/craftsman"
+cleanMavenInstallOfColaInMavenLocalRepository
 
 (
     headInfo "CI: cola-components"
