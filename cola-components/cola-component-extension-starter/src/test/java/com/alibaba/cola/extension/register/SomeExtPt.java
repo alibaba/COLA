3cb429339b590a1a1447ba4e52a17fff0f409a72
凡毅 <fanyi.zfy@alibaba-inc.com>
2021-02-06 03:43:40 +0800

    添加单元测试. #175

---
+++ cola-components/cola-component-extension-starter/src/test/java/com/alibaba/cola/extension/register/SomeExtPt.java
@@ -0,0 +1,8 @@
+package com.alibaba.cola.extension.register;
+
+import com.alibaba.cola.extension.ExtensionPointI;
+
+public interface SomeExtPt extends ExtensionPointI {
+    
+    public void doSomeThing();
+}
