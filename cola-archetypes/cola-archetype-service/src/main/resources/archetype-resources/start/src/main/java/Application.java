dd33f0224b9c276622211773073075f78f68375d
Jerry Lee <oldratlee@gmail.com>
2022-07-31 12:33:42 +0800

    test: fix test error after upgrade `mybatis-spring-boot-starter` to `2.2.2`

--- cola-archetypes/cola-archetype-service/src/main/resources/archetype-resources/start/src/main/java/Application.java
+++ cola-archetypes/cola-archetype-service/src/main/resources/archetype-resources/start/src/main/java/Application.java
@@ -14,7 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Frank Zhang
  */
 @SpringBootApplication
-@MapperScan("${package}.repository")
 public class Application {
 
     public static void main(String[] args) {
