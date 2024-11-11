4d462cf5e7f7b0adb13f8ede34cefd52b63379e3
WUXIN <myeagleandsnake@gmail.com>
2024-11-11 18:22:41 +0800

    feat: add abstract chain action impl

---
+++ cola-components/cola-component-statemachine/src/main/java/com/alibaba/cola/statemachine/impl/AbsChainAction.java
@@ -0,0 +1,29 @@
+package com.alibaba.cola.statemachine.impl;
+
+import com.alibaba.cola.statemachine.Action;
+
+import java.util.Objects;
+
+/**
+ * @author WUXIN
+ * @date 2024/10/14 23:56:10
+ */
+public abstract class AbsChainAction<S,E,C> implements Action<S,E,C> {
+
+    private Action next;
+
+    @Override
+    public void execute(S from, S to, E event, C context) {
+        doExecute(from, to, event, context);
+        if(Objects.nonNull(next)){
+            next.execute(from, to, event, context);
+        }
+    }
+
+    protected abstract void doExecute(S from, S to, E event, C context);
+
+    public AbsChainAction next(Action action){
+        this.next = action;
+        return this;
+    }
+}
